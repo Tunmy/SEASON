08abfe747899a81253014c20b1b17a2db1462efb
Grace Owolabi <gowolabi2@gmail.com>
2019-06-26 03:23:26 -0500

    first commit

---
+++ src/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        
+        <div class="ui active inverted dimmer">
+          <div class="ui text loader">Allow us to read your location</div>
+        </div>
+        
+      
+    );
+};
+
+export default Spinner;
